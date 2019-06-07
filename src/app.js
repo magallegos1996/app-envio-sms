aa9bced35362f7235236683dc2c9daf9a51990b8
magallegos1996 <bwcekos@gmail.com>
2019-06-07 11:23:36 -0500

    Creacion inicial del servidor

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require ('cors');
+
+const app = express();
+
+//Configuracion del servidor
+app.set('port', process.env.PORT || 4000); //Se configura el puerto en el se va a iniciar el servidor de forma dinámica. Tomará el puerto del SO y si no hay usará el puerto por defecto, en este caso el 4000
+app.use(cors());
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json()); //con este metodo nos ayudará a que el servidor pueda entender los datos que vengan desde el navegador. Se tomarán estos datos con req.body
+
+//Routes
+
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+async function main() {
+    //Inicia servidor Express
+    await app.listen(app.get('port'));
+    console.log('Server on port ' + app.get('port') + ': connected' );
+}
+
+//llamamos a la funcion
+main();
+
